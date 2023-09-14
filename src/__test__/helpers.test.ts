85e5b66d8d35def73d81f55680032f2fc85f6a06
bokuzminski <bogdankuzminski@gmail.com>
2023-09-14 11:11:07 +0200

    removed some unnecessary helper tests

--- src/__test__/helpers.test.ts
+++ src/__test__/helpers.test.ts
@@ -3,11 +3,9 @@ import { Move } from "../pathTraversal/pathTraversalModel";
 
 describe("Test helper functions", () => {
   test("should detect incorrect characters", () => {
-    expect(characterIsLetterWeHaveToCollect("He")).toBeFalsy();
     expect(characterIsLetterWeHaveToCollect("Hello")).toBeFalsy();
     expect(characterIsLetterWeHaveToCollect("a")).toBeFalsy();
     expect(characterIsLetterWeHaveToCollect("$")).toBeFalsy();
-    expect(characterIsLetterWeHaveToCollect("%")).toBeFalsy();
     expect(characterIsLetterWeHaveToCollect("x")).toBeFalsy();
     expect(characterIsLetterWeHaveToCollect(" ")).toBeFalsy();
     expect(characterIsLetterWeHaveToCollect("")).toBeFalsy();
@@ -16,7 +14,6 @@ describe("Test helper functions", () => {
   test("should detect correct characters", () => {
     expect(characterIsLetterWeHaveToCollect("A")).toBeTruthy();
     expect(characterIsLetterWeHaveToCollect("B")).toBeTruthy();
-    expect(characterIsLetterWeHaveToCollect("Z")).toBeTruthy();
   });
 
   test("should return UP value", () => {
@@ -49,8 +46,5 @@ describe("Test helper functions", () => {
     expect(() => calculateTheMoveBasedOnIndexes([-1, -1])).toThrowError("Tuple containing the move is wrong");
     expect(() => calculateTheMoveBasedOnIndexes([-1, 2])).toThrowError("Tuple containing the move is wrong");
     expect(() => calculateTheMoveBasedOnIndexes([NaN, NaN])).toThrowError("Tuple containing the move is wrong");
-    expect(() => calculateTheMoveBasedOnIndexes([Infinity, Infinity])).toThrowError(
-      "Tuple containing the move is wrong"
-    );
   });
 });
